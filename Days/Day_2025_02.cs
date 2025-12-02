cddd02bfffe08608a8824a135cdba1b2bf3f0b21
ThibaudTGD <thibaud.ollivier@thegooddrive.net>
2025-12-02 12:04:31 +0100

    Day 02

--- Days/Day_2025_02.cs
+++ Days/Day_2025_02.cs
@@ -13,43 +13,84 @@ public class Day_2025_02 : DayScript2025
 
     protected override string part_1()
     {
+        double result = 0;
         foreach (string instruction in _input.Split(','))
         {
             string[] bounds = instruction.Split('-');
-            Debug.Log(bounds[0].Length + " - " +  bounds[1].Length);
+            Debug.Log("Instruction : " + instruction + "\t | " + bounds[0].Length + " - " +  bounds[1].Length);
 
-            // assume bounds have 1digit difference max
+            // Compute "real" range
+            double minBound, maxBound;
             if (bounds[0].Length % 2 == 0)
-            {
-                Debug.Log("Deal with " + bounds[0]);
-                string pref = bounds[0].Substring(0, bounds[0].Length / 2);
-                Debug.Log("pref " + pref);
-            }
+                minBound = double.Parse(bounds[0].Substring(0, bounds[0].Length / 2));
+            else
+                minBound = System.Math.Pow(10, bounds[0].Length/2);
 
             if (bounds[1].Length % 2 == 0)
+                maxBound = double.Parse(bounds[1].Substring(0, bounds[1].Length / 2));
+            else
+                maxBound = System.Math.Pow(10, bounds[1].Length / 2) -1;
+
+            Debug.Log("Range to check: " + minBound+ "|" +minBound + " - " + maxBound + "|" + maxBound);
+            if (minBound > maxBound)
             {
-                Debug.Log("Deal with " + bounds[1]);
+                Debug.LogWarning("No value in range");
+                continue;
             }
 
-            for (int i = bounds[0].Length; i <= bounds[1].Length; i++)
+            double min = double.Parse(bounds[0]);
+            double max = double.Parse(bounds[1]);
+
+            double curVal;
+            for (double i = minBound; i <= maxBound; i++)
             {
-                if (i % 2 == 1)
-                    continue;
+                string valStr = i.ToString() + i.ToString();
+                curVal = double.Parse(valStr);
+                if (curVal >= min && curVal <= max)
+                {
+                    result += curVal;
+                    Debug.Log("Counted " + curVal);
+                }
 
-
+                if (curVal > max)
+                    break;
             }
         }
 
-        return base.part_1();
+        return result.ToString();
     }
 
     protected override string part_2()
     {
-        foreach (string instruction in _input.Split('\n'))
+        double result = 0;
+        foreach (string instruction in _input.Split(','))
         {
+            string[] bounds = instruction.Split('-');
+            Debug.LogWarning("Instruction : " + instruction + "\t | " + bounds[0].Length + " - " + bounds[1].Length);
+
+            double min = double.Parse(bounds[0]);
+            double max = double.Parse(bounds[1]);
+
+            for (double val = min; val <= max; val++)
+            {
+                string valStr = val.ToString();
+                for (int k = 1; k <= val.ToString().Length /2; k++) //look for possible pattern
+                {
+                    if (valStr.Length % k != 0) // if not multiple, can't be a pattern
+                        continue;
+
+                    string tmp = string.Concat(Enumerable.Repeat(valStr.Substring(0, k), valStr.Length / k));
+                    if (tmp.CompareTo(valStr) == 0)
+                    {
+                        result += val;
+                        Debug.Log("Counted " + val);
+                        break; 
+                    }
+                }
+            }
 
         }
-        
-        return base.part_2();
+
+        return result.ToString();
     }
 }
